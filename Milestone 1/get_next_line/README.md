df3f2eb297d0f5ac8b686962cc55ce460091e134
Tommaso Lanini <tolanini@c2r2p6.42firenze.it>
2025-01-02 15:38:50 +0100

    added get_next_line with bonus

---
+++ Milestone 1/get_next_line/README.md
@@ -0,0 +1,1 @@
+In this project they ask you to create a function that read a single line in a file if u call it again u should have the next line in the file
---
+++ Milestone 1/get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tolanini <tolanini@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/01 18:48:34 by tolanini          #+#    #+#             */
+/*   Updated: 2025/01/02 15:20:33 by tolanini         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+# endif
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+void	ft_bzero(void *s, size_t n);
+
+#endif
