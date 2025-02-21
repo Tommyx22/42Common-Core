5d692bf18620ffeaca12fa22ce7d2841b5a2b2eb
Tommaso Lanini <tolanini@c2r3p8.42firenze.it>
2025-02-21 17:55:43 +0100

    fixed a segmentation fault whenever you close the program, and added a check for the extension of a map

--- Milestone 2/fdf/srcs/main.c
+++ Milestone 2/fdf/srcs/main.c
@@ -6,13 +6,39 @@
 /*   By: tolanini <tolanini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 17:59:49 by tolanini          #+#    #+#             */
-/*   Updated: 2025/02/21 15:17:51 by tolanini         ###   ########.fr       */
+/*   Updated: 2025/02/21 17:47:46 by tolanini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
 static int	expose_handle(t_fdf *fdf);
+
+int	extension_check(char **av)
+{
+	int		l;
+	int		i;
+	char	*str;
+
+	l = 0;
+	i = 3;
+	str = ".fdf";
+	while (av[1][l] != '\0')
+	{
+		l++;
+	}
+	l--;
+	while (i >= 0)
+	{
+		if (av[1][l] != str[i])
+		{
+			return (0);
+		}
+		l--;
+		i--;
+	}
+	return (1);
+}
 
 int	main(int argc, char **argv)
 {
@@ -23,6 +49,11 @@ int	main(int argc, char **argv)
 	{
 		write(1, "Wrong usage. Expected './fdf <file_path>'.\n", 44);
 		exit(0);
+	}
+	if (!extension_check(argv))
+	{
+		write(1, "Wrong extension of file: expected '.fdf'\n", 42);
+		return (0);
 	}
 	file_name = argv[1];
 	fdf = init_fdf(file_name);
